f0757f74ce5a269e1f1ecd80038f59e07b560d17
abdul-dulal <dulal.dpi.387364@gmail.com>
2024-07-10 12:08:42 -0600

    header

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,7 @@
-"use client";
 import { Syne } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/header/header";
-import { useEffect, useState } from "react";
-import Loading from "@/components/ui/Loading";
 
 const syne = Syne({
   weight: ["400", "500", "700"],
@@ -19,28 +16,11 @@ const MyriadPro = localFont({
 });
 
 export default function RootLayout({ children }) {
-  const [isloading, setIloading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIloading(false);
-    }, 2500);
-  }, []);
-
   return (
     <html lang="en" className={` ${syne.className}  `}>
-      <head>
-        <title>Onthi</title>
-      </head>
       <body>
-        {isloading ? (
-          <Loading />
-        ) : (
-          <div>
-            {/* <Header /> */}
-            <div className="">{children}</div>
-          </div>
-        )}
+        <Header />
+        <div className="">{children}</div>
       </body>
     </html>
   );
