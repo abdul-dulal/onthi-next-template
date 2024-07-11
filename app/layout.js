e715f58bf71fbc1c990aa16d9b95dd2ee2ba3428
abdul-dulal <dulal.dpi.387364@gmail.com>
2024-07-11 11:05:48 -0600

    add title

--- app/layout.js
+++ app/layout.js
@@ -5,8 +5,9 @@ import "./globals.css";
 import Header from "@/components/header/header";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Aos from "aos";
+import Loading from "@/components/ui/Loading";
 const syne = Syne({
   weight: ["400", "500", "700"],
   style: ["normal"],
@@ -20,14 +21,26 @@ const MyriadPro = localFont({
 });
 
 export default function RootLayout({ children }) {
+  const [isloading, setIloading] = useState(true);
   useEffect(() => {
+    setTimeout(() => {
+      setIloading(false);
+    }, 1500);
     Aos.init();
-  });
+  }, []);
+
   return (
     <html lang="en" className={`   ${syne.className} `}>
+      <title>Onthi</title>
       <body>
-        <Header />
-        <div className="">{children}</div>
+        {isloading ? (
+          <Loading />
+        ) : (
+          <div>
+            <Header />
+            <div className="">{children}</div>
+          </div>
+        )}
       </body>
     </html>
   );
